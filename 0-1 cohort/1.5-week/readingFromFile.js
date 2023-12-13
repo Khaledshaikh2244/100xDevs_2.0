6dc818fd8fb96602fcb000b5fb393d4664f49c0f
shaikh khaled <khankhaled501@gmail.com>
2023-12-13 14:27:21 +0530

    first

--- 0-1 cohort/1.5-week/readingFromFile.js
+++ 0-1 cohort/1.5-week/readingFromFile.js
@@ -4,5 +4,10 @@ const fs = require ('fs');
 
 fs.readFile("a.txt", "utf-8", function(err, data){
     console.log(data);
+
+// this is also the Async fucntion
 })
+
+
+console.log("hello from the primary file ") // This is will run First 
 
